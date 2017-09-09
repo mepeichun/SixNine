a1c4e778af17c25a2205d99cd6bf4ce388ee75ae
P&C <peichun83@hotmail.com>
2017-09-09 10:26:48 +0800

    Update six_nine.py

--- six_nine.py
+++ six_nine.py
@@ -1,37 +1,65 @@
+
 import pygame
 
-pygame.init()
 
-text = "xx, I love you!"
-font = pygame.font.Font('msyh.ttc', 50)
-rtext = font.render(text, True, (0, 0, 0), (255, 255, 255))
-array = pygame.surfarray.array2d(pygame.transform.scale(rtext, (140, 20)))
+def text2pixel(string, width=140, height=30):
+    """
+    :param string: The words that you want to display
+    :param height: The height of the pixel matrix
+    :param width: The width of the pixel matrix
+    :return: <class 'numpy.ndarray'> with shape (width, height) and type bool
+    """
+    pygame.init()
 
-red = array > 200
+    font = pygame.font.Font('msyh.ttc', 50)
+    rtext = font.render(string, True, (0, 0, 0), (255, 255, 255))
+    array = pygame.surfarray.array2d(pygame.transform.scale(rtext, (width, height))) > 200
 
-content = ''
-shape = red.shape
-print(shape)
-for i in range(shape[1]):
-    for j in range(shape[0]):
-        if red[j][i]:
-            content += '6'
-        else:
-            content += '9'
-    content += '<br />'
-    content += '\n'
+    return array
 
-content += u'\n \n <font size="5">请按下Ctrl + F键，输入数字：6，然后按下回车键！</font>'
 
-with open('test.html', 'w', encoding='utf-8') as f:
-    html_tag_begin = '''
-    <!doctype html><html>
-    <head>
-    <meta charset="utf-8">
-    <title>test</title>
-    </head>
-    <body>
-    '''
-    html_tag_end = '</body>\n</html>'
-    f.write(html_tag_begin + content + html_tag_end)
-    f.close()
+def matrix2six_nine(array):
+    """
+    :param array: <class 'numpy.ndarray'> with shape (width, height) and type bool
+    :return: string with html tag
+    """
+    string = ''
+    shape = array.shape
+    for i in range(shape[1]):
+        for j in range(shape[0]):
+            if array[j][i]:
+                string += '6'
+            else:
+                string += '9'
+        string += '<br />'
+        string += '\n'
+
+    return string
+
+
+def generate_html(from_someone, to_someone, words, title="love"):
+    """
+    :param from_someone: your name as type string
+    :param to_someone:  your girlfriend's/boyfriend's name
+    :param words: what you want to say
+    :param title: the html page title
+    :return: None. But a html file will be generate
+    """
+    html = open('html/template.html', encoding='UTF-8').read()
+    pixel_matrix = text2pixel(words, width=140, height=30)
+    six_nine = matrix2six_nine(pixel_matrix)
+
+    html = html.replace('Your_name', from_someone)
+    html = html.replace('Somebody', to_someone)
+    html = html.replace('Generate text and fill in here!', six_nine)
+    html = html.replace('Replace title here', title)
+
+    with open('html/main.html', 'w', encoding='utf-8') as f:
+        f.write(html)
+        f.close()
+
+
+if __name__ == '__main__':
+
+    # generate_html(from_someone, to_someone, words)
+    generate_html("Peichun", "Elisa", "Elisa, I love you!")
